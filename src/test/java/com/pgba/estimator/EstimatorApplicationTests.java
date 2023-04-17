7105081d929ea080e5946a079642a2e9fe3ea2b4
Robert Hendricks <rmasonh22@gmail.com>
2023-04-17 09:24:28 -0400

    initial tailwind config

---
+++ src/test/java/com/pgba/estimator/EstimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgba.estimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstimatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
